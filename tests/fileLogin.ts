0891bf6bbfbccb732143abd916340e0061a6e691
pvanquang <intern.lhoangdung@tma.com.vn>
2023-06-22 10:24:45 +0700

    update login

--- tests/fileLogin.ts
+++ tests/fileLogin.ts
@@ -5,29 +5,29 @@ test.describe("Test case login", async () => {
   test.beforeEach(async ({ page,baseURL}) => {
    await page.goto(`${baseURL}web/index.php/auth/login`);
 });
-  data.tc.forEach((element:User,index:number) => {
-    test("Happy case: "+index.toString(), async ({page,baseURL}) => {
+  // data.tc.forEach((element:User,index:number) => {
+    test("Happy case: ", async ({page,baseURL}) => {
       const admin = new Admin(page);
       await page.goto(`${baseURL}web/index.php/auth/login`);
-      await admin.Login.loginId(element.username,element.password);
+      await admin.Login.loginId("Admin","admin123");
       let logo = await page.locator("xpath=//img[@src='/web/images/orangehrm-logo.png?v=1683010990518']");
       await expect(logo).toBeVisible();
     })
-  });
-  data.tc_unfill.forEach((element:User,index:number) => {
-    test("Unfill unsername & password: "+index.toString(), async ({ page}) => {
+  // });
+  // data.tc_unfill.forEach((element:User,index:number) => {
+    test("Unfill unsername & password: ", async ({ page}) => {
       const admin = new Admin(page);;
-      await admin.Login.loginId(element.username,element.password);
+      await admin.Login.loginId("1123asd","asdasd1");
       let ms ="Required";
       expect("Required").toBe(ms);
     })
-  });
-  data.tc_false.forEach((element:User,index:number) => {
-    test("Fill unsername & password invalid: "+index.toString(), async ({ page}) => {
+  // });
+  // data.tc_false.forEach((element:User,index:number) => {
+    test("Fill unsername & password invalid: ", async ({ page}) => {
       const admin = new Admin(page);;
-      await admin.Login.loginId(element.username,element.password);
+      await admin.Login.loginId("asd","123");
       let ms = await page.locator("xpath=//*[contains(p,'Invalid credentials')]//child::p").textContent();
       await expect(ms).toBe("Invalid credentials");
     })
-  });
+  // });
 })
